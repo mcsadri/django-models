31943a092ff7003ce3cd89920be06c6bdc0c824d
Manuch Sadri <colonquebang@hotmail.com>
2023-06-21 16:12:52 -0700

    updates project by adding initial VUT (views, urls, templates) for basic list view of snack_tracker_project

--- snacks/views.py
+++ snacks/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Snack
 
-# Create your views here.
+
+class SnackListView(ListView):
+    template_name = "snack_list.html"
+    model = Snack
+    context_object_name = "snacks"
+
+
+class SnackDetailView(DetailView):
+    pass
