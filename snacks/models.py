f17f3265368df4da55bfc2194293f9558ec54751
Manuch Sadri <colonquebang@hotmail.com>
2023-06-21 21:50:22 -0700

    adds objects class variable to Snack class in models.py

--- snacks/models.py
+++ snacks/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth import get_user_model
 # each class = a table in db
 class Snack(models.Model):
     # class vars for each table col/field using Model Field Reference (field types + field options (keyword arguments))
+    objects = None
     name = models.CharField(max_length=64)
     purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     description = models.TextField(max_length=255, default="snack description")
